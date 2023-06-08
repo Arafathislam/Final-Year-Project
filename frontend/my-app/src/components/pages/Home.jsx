c9690572652b5c9584bb1df7072ba8b3bb55fd70
Arafathislam <islamarafath315@gmail.com>
2023-06-08 15:04:37 +0600

    working

---
+++ frontend/my-app/src/components/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <>
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae cumque voluptate repellendus molestiae, necessitatibus minima corrupti obcaecati pariatur illo iste cupiditate quia est.</p>
+   </>
+  )
+}
+
+export default Home
